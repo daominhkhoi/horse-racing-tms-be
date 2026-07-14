bd86c74d5b13bbe0805599091d9cd00b867d8b68
DaoMinhKhoi <daokhoi3934@gmail.com>
2026-07-14 11:19:05 +0700

    add module mark as completed and input result for ad & ref

---
+++ HorseRacingTournamentManagementSystem_0/Modules/Races/Interfaces/IRaceService.cs
@@ -0,0 +1,13 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using HorseRacingTournamentManagementSystem_0.Modules.Races.DTOs;
+
+namespace HorseRacingTournamentManagementSystem_0.Modules.Races.Interfaces
+{
+    public interface IRaceService
+    {
+        Task<bool> UpdateRaceStatusAsync(int raceId, string newStatus);
+        Task<bool> SubmitRaceResultsAsync(int raceId, SubmitRaceResultDto dto);
+        Task<List<ResultViewDto>> GetRaceResultsAsync(int raceId);
+    }
+}
