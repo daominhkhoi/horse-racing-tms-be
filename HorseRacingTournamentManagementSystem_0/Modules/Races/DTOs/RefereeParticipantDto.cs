a133b19dfd8d71e0ea8689dd5f6bd97109d81526
daominhkhoi <daokhoi3934@gmail.com>
2026-07-31 19:39:03 +0700

    fix leaderboard ui

--- HorseRacingTournamentManagementSystem_0/Modules/Races/DTOs/RefereeParticipantDto.cs
+++ HorseRacingTournamentManagementSystem_0/Modules/Races/DTOs/RefereeParticipantDto.cs
@@ -4,6 +4,9 @@ namespace HorseRacingTournamentManagementSystem_0.Modules.Races.DTOs
     {
         public int ParticipantId { get; set; }
         public string HorseName { get; set; } = null!;
+        public int HorseId { get; set; }
+        public string? HorseAvatar { get; set; }
         public string JockeyName { get; set; } = null!;
+        public int? LaneNumber { get; set; }
     }
 }
